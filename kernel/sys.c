a0677b3175888a5c37d51b27a8679470aca69b9c
GoRhanHee <gorhanhee01@gmail.com>
2025-08-02 21:43:43 +0900

    kernel: Implement SuSFS v.1.5.9 (BACKPORT)

--- kernel/sys.c
+++ kernel/sys.c
@@ -1240,6 +1240,9 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1269,6 +1272,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	}
 #endif
 	up_read(&uts_sem);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
